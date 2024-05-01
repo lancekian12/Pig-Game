425962f1fdb76a53d9315a8e412e9fae6ee864ec
Lance Kian <lafa.flores.up@phinmaed.com>
2024-05-02 00:15:57 +0800

    Added new game functionality

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
+const btnNewGame = document.querySelector('.btn--new');
 
 // Starting Conditions
 score0El.textContent = 0;
@@ -58,7 +59,7 @@ btnHold.addEventListener('click', function () {
     document.getElementById(`score--${activePlayer}`).textContent =
       scores[activePlayer];
     //2. Check if player's score is >= 100
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= 50) {
       // Finish the game
       playing = false;
       diceEl.classList.add('hidden');
@@ -74,4 +75,27 @@ btnHold.addEventListener('click', function () {
       switchPlayer();
     }
   }
+});
+
+btnNewGame.addEventListener('click', function () {
+  //Reseting the game
+
+  //MyAnswer
+  //   scores[0] = 0;
+  //   scores[1] = 0;
+  //   document.getElementById(`score--0`).textContent = scores[0];
+  //   document.getElementById(`score--1`).textContent = scores[1];
+  //   currentScore = 0;
+  //   document.querySelector('#current--0').textContent = currentScore;
+  //   document.querySelector('#current--1').textContent = currentScore;
+
+  //Solution
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
 });
